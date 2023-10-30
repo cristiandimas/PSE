79905be6a5a46861101b09f8ab8427dbce56ee5e
Cristian Barrantes <cristiandimas74@gmail.com>
2023-10-30 12:57:06 -0500

    iniciando

---
+++ Code.aspx
@@ -0,0 +1,138 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Code.aspx.cs" Inherits="Code" %>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>EC Cargo</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/agency.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+
+    <script type="text/javascript" src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
+
+<body class="logueo">
+   
+    <div>
+        <div class="row">
+            <img class="img-responsive center-block" src="img/LOGO.png" style="padding-top: 150px;">
+        </div>
+        <br>
+
+        <!--div pagos-->
+        <div class="col-lg-12">
+            <img class="img-responsive center-block " style="" src="img/pagos2.png">
+        </div>
+
+        <div class="container">            
+            <form runat="server">
+                <asp:Panel ID="Panel2" runat="server">
+                    <asp:Login ID="Login2" runat="server" OnAuthenticate="LogAcceso_Authenticate2" DestinationPageUrl="~/Code.aspx">
+                        <LayoutTemplate>
+                            <!--login-->
+                            <div class="top-content">
+                                <div class="inner-bg">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-sm-6 col-sm-offset-3 form-box">
+                                                <div class="form-bottom">
+                                                    <div class="row">
+                                                        <asp:TextBox ID="UserName" runat="server"  visible="false"  CssClass="form-control" Style="border-radius: 25px; height: 45px;" Placeholder="Código de autenticación" Width="100%"></asp:TextBox>
+                                                    </div>
+                                                   
+                                                    <br />
+
+                                                    <div class="row"  >                                                        
+                                                        <asp:TextBox ID="Password" runat="server" TextMode="Password" CssClass="form-password form-control" Style="border-radius: 25px;  height: 45px;" Placeholder="Código de autenticación" Width="100%"></asp:TextBox>
+                                                    </div>
+                                                    <asp  runat="server" ControlToValidate="Password"   ValidationGroup="Login2" Style="color:#ffffff"></asp>
+
+                                                    <br>
+
+                                                    <div>
+                                                        <asp:Button ID="LoginButton2" runat="server" CommandName="Login" CssClass="botonimagen center-block" ImageUrl="img/ingresar.png" ValidationGroup="Login2" Style="border-radius: 25px; height: 40px; width: 220px;" />
+                                                    </div>
+
+                                                    <br />
+
+                                                    
+                                                    <div>
+                                                        <br>
+                                                        <a href="manuales/instructivo.pdf">
+                                                            <img src="img/instructivo.png" class="img-responsive center-block"></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                   
+                        </LayoutTemplate>
+                    </asp:Login>
+
+                    <br />
+
+                    <div class="alert alert-info">
+                        <strong>Atención!</strong>
+                        Si es primera vez que ingresa, debe colocar como usuario el NIT de la empresa a la que pertenece, incluyendo el dígito de verificación y una contraseña cualquiera, luego, se le solicitará registrar una dirección de correo electrónico y una nueva contraseña.
+                    </div>
+
+                    <div runat="server" class="alert alert-danger" id="div_error" visible="false">
+                        <strong>Error!</strong>
+                        <asp:Label ID="Label2" runat="server"></asp:Label>
+                    </div>
+                </asp:Panel>         
+            </form>
+        </div>
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <!--footer-->
+    <div class="col-lg-12">
+        <img class="img-responsive center-block "  src="img/pie2.png" style="width: 2000px">
+    </div>
+    <br>
+    <br>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/agency.js"></script>
+
+</body>
+
+</html>
+
---
+++ Code.aspx.cs
@@ -0,0 +1,183 @@
+﻿using AjaxControlToolkit.HtmlEditor.ToolbarButtons;
+using System;
+using System.Web;
+using System.Web.UI.WebControls;
+
+public partial class Code : System.Web.UI.Page
+{
+    private static int _admin = 2;
+    private static int _cliente = 1;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (HttpContext.Current.User.Identity.IsAuthenticated)
+        {
+            USUARIOSWEB usuario = (USUARIOSWEB)Session["usuario"];
+            TERCEROS cliente = (TERCEROS)Session["cliente"];
+            fed_usuarios admin = (fed_usuarios)Session["admin"];
+
+            if (admin != null) Response.Redirect("Admin/Inicio.aspx");
+            else if (usuario == null || cliente == null)
+            {
+                return;
+            }
+            //else
+            //{
+            //    if (usuario.IDTIPOUSUARIOWEB == _cliente)
+            //        Response.Redirect("Interno/Inicio.aspx");
+            //}
+        }
+    }
+
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void LogAcceso_Authenticate2(object sender, AuthenticateEventArgs e)
+    {
+        div_error.Visible = false;
+        Label2.Text = "";
+        string codeSave = Session["CodigoVerificacion"] as string;
+        string userConGuion = ManejaUsuarios.LimpiarNIT(Login2.UserName.Trim());
+        string userSinGuion = String.Empty;
+
+        // Verificar si tiene dígito de verificación
+        if (Login2.UserName.IndexOf('-') != -1)
+            userSinGuion = ManejaUsuarios.LimpiarNIT(Login2.UserName.Remove(Login2.UserName.IndexOf('-')));
+        else
+            userSinGuion = ManejaUsuarios.LimpiarNIT(Login2.UserName.Trim());
+        //
+
+        string nitEC = Login2.UserName.Trim();
+        string code = Login2.Password.Trim();
+
+        ManejaUsuarios manejaUsuarios = new ManejaUsuarios();
+
+        forward_clientes clienteEC = manejaUsuarios.BuscarClienteBD_ECCargo(nitEC);
+
+        TERCEROS clienteConGuion = manejaUsuarios.BuscarClienteBDHosting(userConGuion);
+        TERCEROS clienteSinGuion = manejaUsuarios.BuscarClienteBDHosting(userSinGuion);
+        fed_usuarios admin = manejaUsuarios.BuscarAdmin(Login2.UserName.Trim());
+
+        if (admin != null)
+        {
+            var admin_password = manejaUsuarios.BuscarAdminPassword(Login2.UserName.Trim());
+
+            if (admin_password != null && admin_password.fed_usuario_intentos < ManejaUsuarios.numero_intentos)
+            {
+                Session["admin"] = admin_password;
+                Session["usuario"] = null;
+                Session["cliente"] = null;
+                manejaUsuarios.ReiniciarIntentosAdmin(admin_password);
+                manejaUsuarios.DesconectarBD();
+                e.Authenticated = true;
+            }
+            else if (admin.fed_usuario_intentos >= ManejaUsuarios.numero_intentos)
+            {
+                manejaUsuarios.DesconectarBD();
+                div_error.Visible = true;
+                Label2.Text = "Su usuario ha sido bloqueado al exceder el límite de inicios de sesión fallidos.";
+            }
+            else
+            {
+                manejaUsuarios.ActualizarIntentosFallidosAdmin(admin);
+                manejaUsuarios.DesconectarBD();
+                div_error.Visible = true;
+                Label2.Text = "Los datos ingresados son incorrectos.";
+            }
+        }
+        else if (clienteEC != null)
+        {
+            if (clienteConGuion != null || clienteSinGuion != null)
+            {
+                USUARIOSWEB usuario = manejaUsuarios.BuscarUsuarioBDHosting(userConGuion ?? String.Empty);
+
+                if(usuario == null) usuario = manejaUsuarios.BuscarUsuarioBDHosting(userSinGuion ?? String.Empty);
+
+                if (usuario != null)
+                {
+                    
+
+                    USUARIOSWEB usuario_password = manejaUsuarios.BuscarUsuarioYPasswordBDHosting(userConGuion ?? String.Empty);
+
+                    if (usuario_password == null) usuario_password = manejaUsuarios.BuscarUsuarioYPasswordBDHosting(userSinGuion ?? String.Empty);
+
+                    if (code == codeSave)
+                    {
+                        Session["admin"] = null;
+                        Session["usuario"] = usuario_password;
+                        Session["cliente"] = usuario_password.TERCEROS;
+                        manejaUsuarios.ReiniciarIntentos(usuario_password);
+                        manejaUsuarios.DesconectarBD();
+                        e.Authenticated = true;
+                        Response.Redirect("Interno/Inicio.aspx");
+
+                    }
+                    else if (usuario.NUMEROINTENTOS >= ManejaUsuarios.numero_intentos)
+                    {
+                        
+                        manejaUsuarios.DesconectarBD();
+                        div_error.Visible = true;
+                        Label2.Text = "Su usuario ha sido bloqueado al exceder el límite de inicios de sesión fallidos.";
+                    }
+                    else
+                    {
+                        manejaUsuarios.ActualizarIntentosFallidos(usuario);
+                        manejaUsuarios.DesconectarBD();
+                        div_error.Visible = true;
+                        Label2.Text = "Los datos ingresados son incorrectos.";
+                    }
+                }
+                else
+                {
+                    Session["cliente"] = clienteConGuion ?? clienteSinGuion;
+                    Session["registrar"] = "S";
+                    Session["clicodigo"] = clienteEC.cli_codigo.ToString();
+                    manejaUsuarios.DesconectarBD();
+                    Response.Redirect("RegistroUsuarios.aspx");
+                }
+            }
+            else
+            {
+                clienteConGuion = manejaUsuarios.IngresarClienteNoExistente(clienteEC);
+
+                if (clienteConGuion != null)
+                {
+                    Session["cliente"] = clienteConGuion;
+                    Session["registrar"] = "S";
+                    Session["clicodigo"] = clienteEC.cli_codigo.ToString();
+                    manejaUsuarios.DesconectarBD();
+                    Response.Redirect("RegistroUsuarios.aspx");
+                }
+            }
+        }
+        else
+        {
+            //El cliente no existe en ningún lado.
+            manejaUsuarios.DesconectarBD();
+            div_error.Visible = true;
+            Label2.Text = Session["CodigoVerificacion"] as string;
+        }
+    }    
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void LinkButton3_Click(object sender, EventArgs e)
+    {
+        //Elementos del panel 1
+        Panel2.Visible = false;
+        div_error.Visible = false;
+        Response.Redirect("RecuperarContrasena.aspx");
+    }
+}
